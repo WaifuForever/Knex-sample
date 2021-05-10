8a6e62ab3737b4cdb0b9b2d1681dbc13a131a70b
Waifu_Forever <itachiut1r4@gmail.com>
2021-05-10 01:56:27 -0300

    seed fixed

--- backend/src/database/seeds/001_users.js
+++ backend/src/database/seeds/001_users.js
@@ -1,6 +1,6 @@
 
-exports.seed = knex => 
-  knex.schema.hasTable('teams').then(exists => {
+exports.seed = function(knex) {
+  return knex.schema.hasTable('teams').then(function(exists) {
     if (!exists) {
       knex.schema.createTable('teams', table => {
         table.increments('team_id')
@@ -8,25 +8,28 @@ exports.seed = knex =>
         table.timestamp('created_at').defaultTo(knex.fn.now())
         table.timestamp('updated_at').defaultTo(knex.fn.now())
       });
-      knex('teams').insert([
-        {id: 1, name: 'rowValue1'},
-        {id: 2, name: 'rowValue2'},
-        {id: 3, name: 'rowValue3'}
-      ]);
       console.log('doesnt exists')
+      return knex('teams').insert([
+        { name: 'rowValue1' },
+        { name: 'rowValue2' },
+        { name: 'rowValue3' }
+      ]);
+      
     }
     else {
+      console.log('exists')
       // Deletes ALL existing entries
-      knex('teams').del().then(function() {
+      return knex('teams').del().then(function() {
         // Inserts seed entries
         return knex('teams').insert([
-          {id: 1, name: 'rowValue1'},
-          {id: 2, name: 'rowValue2'},
-          {id: 3, name: 'rowValue3'}
+          { name: 'rowValue1' },
+          { name: 'rowValue2' },
+          { name: 'rowValue3' }
         ]);
        
       });
-      console.log('exists')
+      
     }
   });
+}
   
