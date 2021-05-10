d5c01a064882d8080ee082b31c6f6fbf64218284
Waifu_Forever <itachiut1r4@gmail.com>
2021-05-10 01:43:40 -0300

    fixing seeds

--- backend/src/database/seeds/001_users.js
+++ backend/src/database/seeds/001_users.js
@@ -2,7 +2,7 @@
 exports.seed = knex => 
   knex.schema.hasTable('teams').then(exists => {
     if (!exists) {
-      knex.schema.createTable('users', table => {
+      knex.schema.createTable('teams', table => {
         table.increments('team_id')
         table.text('name').notNullable()    
         table.timestamp('created_at').defaultTo(knex.fn.now())
@@ -13,17 +13,20 @@ exports.seed = knex =>
         {id: 2, name: 'rowValue2'},
         {id: 3, name: 'rowValue3'}
       ]);
+      console.log('doesnt exists')
     }
     else {
       // Deletes ALL existing entries
-      knex('teams').del().then(() => {
+      knex('teams').del().then(function() {
         // Inserts seed entries
-        knex('teams').insert([
+        return knex('teams').insert([
           {id: 1, name: 'rowValue1'},
           {id: 2, name: 'rowValue2'},
           {id: 3, name: 'rowValue3'}
         ]);
+       
       });
+      console.log('exists')
     }
   });
   
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,19 @@ const express = require('express');
 
 const routes = express.Router()
 
+const knex = require('./database')
 
 routes.get('/', (req, res) => {
-    return res.jsonOK( null, "Hello World", null);
+    return res.json("Hello World");
 
 });
+
+routes.get('/teams', (req, res) => {
+    knex('teams').then((result) => {
+        console.log(result)
+        res.json(result)
+    })
+})
+
+
 module.exports = routes;
