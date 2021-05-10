e296d6b55380e2a3042ee5e6603289416e46aba6
Waifu_Forever <itachiut1r4@gmail.com>
2021-05-10 16:22:02 -0300

    country migration updated

--- backend/src/database/migrations/20210510051013_create_table_countries.js
+++ backend/src/database/migrations/20210510051013_create_table_countries.js
@@ -2,9 +2,7 @@
 exports.up = knex => knex.schema.createTable('countries', table => {
     table.increments('country_id')    
     table.text('name').notNullable()
-    
-    table.timestamp('created_at').defaultTo(knex.fn.now())
-    table.timestamp('updated_at').defaultTo(knex.fn.now())
+   
 })
   
 
