480f369887a88651b875a3dad25ccb26ad92c420
Waifu_Forever <itachiut1r4@gmail.com>
2021-05-10 16:21:40 -0300

    CountryController

---
+++ backend/src/controllers/CountryController.js
@@ -0,0 +1,55 @@
+const knex = require('../database')
+
+module.exports = {
+    async store (req, res){
+       
+        try {
+            const { name } = req.body
+            console.log(req.body)
+
+            knex.select("name")
+                .from("countries")
+                .where("name", name)              
+                .then(namesList => {
+                    if (namesList.length === 0) {
+
+                        return knex('countries').insert({               
+                            name: name
+                
+                        }).then(() => {
+                            res.status(201).json("Country added!!")
+                        });
+
+                      
+                    }
+                    else {
+                        return res.json({error: "Country already registered!!"});
+                    }
+                }).catch(err => {
+                    return res.status(500).json({error: "Error"});
+                
+                })                    
+
+            
+        } catch (error) {
+            return res.json(error)
+        }
+       
+    },
+
+    async index(req, res){
+        
+        try {
+            const results = await knex('countries')
+                   
+            return res.json(results)
+        } catch (err){
+            console.log(err)
+            return res.json(
+                {err: err, message: "Error"}
+            )
+        }
+    }
+    
+
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,19 +3,21 @@ const routes = express.Router()
 
 const PilotController = require('./controllers/PilotController')
 const TeamController = require('./controllers/TeamController')
-
+const CountryController = require('./controllers/CountryController')
 
 routes.get('/', (req, res) => {
     return res.json("Hello World");
 });
 
 routes.post('/teams', TeamController.store);
-
-routes.post('/pilots', PilotController.store);
-
 routes.get('/teams', TeamController.index);
 
-routes.get('/pilots', PilotController.index);
 
+routes.post('/pilots', PilotController.store);
+routes.post('/pilots', PilotController.store);
+
+
+routes.get('/countries', CountryController.index);
+routes.post('/countries', CountryController.store);
 
 module.exports = routes;
